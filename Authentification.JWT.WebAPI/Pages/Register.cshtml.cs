d2429df764ef8e69dec67aa6f2ae73579c7408f6
anashamza49 <anass_hamzaoui@hotmail.com>
2025-04-17 19:26:02 +0200

    updating razor for test

--- Authentification.JWT.WebAPI/Pages/Register.cshtml.cs
+++ Authentification.JWT.WebAPI/Pages/Register.cshtml.cs
@@ -1,12 +1,34 @@
+using Authentification.JWT.Service.DTOs;
+using Authentification.JWT.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Authentification.JWT.WebAPI._Pages
 {
-    public class RegisterModel : PageModel
+    public class RegisterModel(IUserService userService) : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public RegisterDto Register { get; set; }
+
+        public string? ErrorMessage { get; set; }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid)
+                return Page();
+
+            try
+            {
+                var user = await userService.RegisterUserAsync(Register.Username, Register.Email, Register.Password);
+                return RedirectToPage("/Login");
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+                return Page();
+            }
         }
     }
 }
